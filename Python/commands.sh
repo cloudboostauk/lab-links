ece941e5b9a74292d92b09f366d8f8a680394c9e
safeeha <eemarabs@gmail.com>
2025-08-26 17:34:21 +0100

    new commands

---
+++ Python/commands.sh
@@ -0,0 +1,2 @@
+
+print(name, "\n", age, "\n", hobby, "\n", workplace, "\n", state_of_origin, "\n", current_residence)
---
+++ Terraform/commands.sh
@@ -0,0 +1,9 @@
+# Task 1 Step 1
+aws s3api create-bucket \
+ --bucket <your-unique-bucket-name> \
+ --region <your-region>
+
+
+
+# Task 1 Step 3
+mkdir cba_terraform2 && cd cba_terraform2
