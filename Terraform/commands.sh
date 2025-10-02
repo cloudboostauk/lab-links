34560e46247c8313bd354e72c1173949aebe4184
safeeha <eemarabs@gmail.com>
2025-10-02 13:51:36 +0100

    added variables.tf

--- Terraform/commands.sh
+++ Terraform/commands.sh
@@ -1,4 +1,5 @@
 # Task 1 Step 1
 aws s3api create-bucket \
- --bucket <your-unique-bucket-name> \
- --region <your-region>
+--bucket <your-unique-bucket-name> \
+--region <your-region> \
+--create-bucket-configuration LocationConstraint=<your-region>
