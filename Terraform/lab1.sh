f4fdd04f1b91f9a5554d188a1794c8422a9b9f80
safeeha <eemarabs@gmail.com>
2025-10-02 13:36:38 +0100

    added variables.tf

---
+++ Terraform/lab1.sh
@@ -0,0 +1,20 @@
+
+variable "region" {
+  default = "eu-west-2"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "instance_ami" {
+  default = "replace with a linux ami id from your aws console"
+}
+
+variable "vpc_id" {
+  default = ""
+}
+
+variable "key_name" {
+  default = "replace with a key-pair name from your aws console"
+}
