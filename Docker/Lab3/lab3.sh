7c346f70e769900a2a974ed64adff55eacf03af7
safeeha <eemarabs@gmail.com>
2025-10-17 10:31:12 +0100

    new userdata

--- Docker/Lab3/lab3.sh
+++ Docker/Lab3/lab3.sh
@@ -1,8 +1,8 @@
 # Task 1 Step 4
-docker run -it --name my-volume-test -v data-volume:/data rockylinux:9 /bin/bash 
+docker run -it --name my-volume-test -v data-volume:/data centos:8 /bin/bash 
 
 # Task 2 Step 8
-docker run -it -v /home/ubuntu/docker-folder:/var/myfiles/ rockylinux:9 /bin/bash
+docker run -it -v /home/ubuntu/docker-folder:/var/myfiles/ centos:8 /bin/bash
 
 # Task 4 Step 4a
 docker run -itd --name container1 ubuntu-with-ping
