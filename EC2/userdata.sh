3db8b5558197412f935f50be22ab81ec32fc8abb
safeeha <eemarabs@gmail.com>
2025-11-06 09:09:29 +0100

    added EC2 userdata

--- EC2/userdata.sh
+++ EC2/userdata.sh
@@ -1,51 +1,38 @@
-#!/bin/bash
+#Note: This userdata is for Ubuntu AMI, so select Ubuntu as AMI while configuring your EC2
+#please ignore the comments, copy and use the commands only
+
+#command to update the OS
 sudo apt-get update -y 
 
+#command to add the Jenkins repo
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
+#command to Import a key file from Jenkins-CI to enable installation from the package
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
+#command to update the OS
 sudo apt-get update -y 
+
+#command to install java
 sudo apt install openjdk-17-jre -y 
+
+#command to install Jenkins
 sudo apt-get install jenkins -y
 
-
+#command to update the OS
 apt-get update -y
-apt-get install python3.8 -y
-apt-get install -y python3-pip
 
+#command to enable Jenkins
+sudo systemctl enable jenkins
 
-sudo apt update
-sudo apt install maven -y
+#command to start Jenkins
+sudo systemctl start jenkins
 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+#command to check Jenkins status
+sudo systemctl status jenkins
 
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-sudo apt-get update && \
-sudo apt-get install docker.io -y
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-
-sudo usermod -a -G docker ubuntu
-sudo chmod 666 /var/run/docker.sock
-sudo usermod -a -G docker jenkins
-sudo service docker start
-sudo service jenkins restart
-sudo systemctl daemon-reload
-sudo service docker restart
-
-
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-sudo apt update && sudo apt install terraform -y
-
-sudo apt  install awscli -y
-curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo apt install unzip -y
-unzip awscliv2.zip
-sudo ./aws/install --update
+#command to get Jenkins password
+cat /var/lib/jenkins/secrets/initialAdminPassword
