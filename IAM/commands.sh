1d567bb85c217fe89afec5bb9594e44097715c15
safeeha <eemarabs@gmail.com>
2025-11-16 13:51:21 +0100

    ssh-pub-key

---
+++ IAM/commands.sh
@@ -0,0 +1,6 @@
+aws ec2-instance-connect send-ssh-public-key \
+    --instance-id PRIVATE-INSTANCE \
+    --region eu-west-2 \
+    --availability-zone eu-west-2a \
+    --instance-os-user ec2-user \
+    --ssh-public-key file:///home/ssm-user/.ssh/id_rsa.pub
