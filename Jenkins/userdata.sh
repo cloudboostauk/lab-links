fbbb5032fa7293503592ed7f3174665b2d7a0db6
safeeha <eemarabs@gmail.com>
2026-04-16 11:41:11 +0100

    updated java version

--- Jenkins/userdata.sh
+++ Jenkins/userdata.sh
@@ -1,37 +1,48 @@
-#!/bin/bash
+#!/bin/bash                                                                   
+   
+  # Update system                                                               
+  sudo apt-get update -y                                          
 
-# Jenkins repo 
-sudo apt-get update -y
+  # Install Java 21 (required for Ubuntu 24.04)                                 
+  sudo apt-get install -y openjdk-21-jdk
+                                                                                
+  # Jenkins repo                                                                
+  sudo wget -O /usr/share/keyrings/jenkins-keyring.asc
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key                      
+                                                                  
+  echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]                 
+  https://pkg.jenkins.io/debian-stable binary/" | sudo tee        
+  /etc/apt/sources.list.d/jenkins.list > /dev/null                              
+                                                                  
+  sudo apt-get update -y
+  sudo apt-get install -y jenkins
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key \
-  | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+  # Maven                                                                       
+  sudo apt-get install -y maven
+                                                                                
+  # Docker                                                        
+  sudo apt-get install -y docker.io
+  sudo usermod -aG docker ubuntu                                                
+  sudo usermod -aG docker jenkins                                               
+  sudo systemctl enable docker                                                  
+  sudo systemctl start docker                                                   
+                                                                  
+  # Start Jenkins properly as system service
+  sudo systemctl enable jenkins
+  sudo systemctl start jenkins
 
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" \
-  | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+  # Terraform
+  wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o
+  /usr/share/keyrings/hashicorp-archive-keyring.gpg                             
+  echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] 
+  https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee        
+  /etc/apt/sources.list.d/hashicorp.list                          
+  sudo apt-get update -y                                                        
+  sudo apt-get install -y terraform                               
 
-sudo apt-get update -y
-sudo apt-get install -y openjdk-17-jre jenkins
-
-sudo apt update
-sudo apt install maven -y
-
-# Docker
-sudo apt-get install -y docker.io
-sudo usermod -aG docker ubuntu
-sudo usermod -aG docker jenkins || true
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo systemctl restart jenkins
-
-#Terraform
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-sudo apt update && sudo apt install terraform -y
-
-#AWS cli
-sudo apt-get install -y unzip
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip -o awscliv2.zip
-sudo ./aws/install --update
+  # AWS CLI
+  sudo apt-get install -y unzip
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o            
+  "awscliv2.zip"
+  unzip -o awscliv2.zip                                                         
+  sudo ./aws/install --update
