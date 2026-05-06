1e2c654cf7e083fed22b9677df50ca7bf5f2259e
safeeha <eemarabs@gmail.com>
2026-05-06 10:16:15 +0100

    added resource

--- Terraform/Lab4/resources.tf
+++ Terraform/Lab4/resources.tf
@@ -40,3 +40,17 @@ resource "aws_sns_topic" "system_alerts" {
     Purpose     = "infrastructure-monitoring"
   }
 }
+
+
+resource "aws_sns_topic" "compliance" {
+  name = "${local.name_prefix}-compliance"
+
+  kms_master_key_id = var.enable_encryption ? "alias/aws/sns" : null
+
+  tags = {
+    Name        = "${local.name_prefix}-compliance"
+    Environment = var.environment
+    Purpose     = "regulatory-compliance"
+    Critical    = "true"
+  }
+}
